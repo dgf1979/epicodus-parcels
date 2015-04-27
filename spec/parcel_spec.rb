8ab439d1d04360a880d8c27a842a009b055f4222
Drew & Jesse James <student@epicodus.com>
2015-04-27 12:22:44 -0700

    write unit spec for calculating package size for shipping cost

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -15,9 +15,15 @@ describe('Parcel#volume') do
 end
 
 describe('Parcel#cost_to_ship') do
-  it('calculate cost of shipping given weight and volume') do
+  it('calculate cost of shipping given weight times 1.5') do
     parcel1 = Parcel.new(4, 4, 4, 20)
     expect(parcel1.cost_to_ship()).to(eq(30))
   end
 
+describe('Parcel#shipping_size')
+  it('calculate package size for use in shipping cost calculation') do
+    parcel1 = Parcel.new(4, 4, 4, 20)
+    expect(parcel1.shipping_size()).to(eq("small"))
+  end
+  
 end
