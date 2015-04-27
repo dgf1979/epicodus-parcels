96e0baa992cd51f5ee208935c111b7eea6091297
Drew & Jesse James <student@epicodus.com>
2015-04-27 12:16:00 -0700

    write unit test to calculate shipping cost based on weight and volume

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -13,3 +13,11 @@ describe('Parcel#volume') do
     expect(parcel1.volume()).to(eq(64))
   end
 end
+
+describe('Parcel#cost_to_ship') do
+  it('calculate cost of shipping given weight and volume') do
+    parcel1 = Parcel.new(4, 4, 4, 20)
+    expect(parcel1.cost_to_ship()).to(eq(30))
+  end
+
+end
