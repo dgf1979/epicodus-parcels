db8deae031b4ee8473a7d31864218b8b28e9fc85
Drew & Jesse James <student@epicodus.com>
2015-04-27 14:34:09 -0700

    integration test: erify cost given a package size with no extras

--- spec/parcel_integration_spec.rb
+++ spec/parcel_integration_spec.rb
@@ -5,8 +5,20 @@ set(:show_exceptions, false)
 
 describe('Parcel Integration Testing', {:type => :feature}) do
   #framework test
-  it('verifies a test route is getting to a test layout page') do
+  it('verifies a test route is getting to its target view') do
     visit('/')
     expect(page).to have_content('Parcel Service')
   end
+
+  #verify integration of parcel shipping cost
+  it('verifies expected cost for a given package size and weight') do
+    visit('/')
+    fill_in('length', :with => 4)
+    fill_in('width', :with => 4)
+    fill_in('height', :with => 4)
+    fill_in('weight', :with => 20)
+    click_button('Send')
+    expect(page).to have_content('20')
+  end
+
 end
